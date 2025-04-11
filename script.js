7f365cff4267ae356434f6f9c2641615cafbf9a7
egonspengler24 <98477110+egonspengler24@users.noreply.github.com>
2025-04-11 15:04:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 const locations = [
+    { name: "1. The Commodore Inn", lat: 54.228517801041455, lng: -2.9093019885160087, description: "breakfast 9am to 10am, lunch 12pm to 2:30pm" },
+    { name: "2. The Screes Inn", lat: 54.45587337424352, lng: -3.35150170105879, description: "breakfast 7am - 9am, lunch from 12pm." }, 
+    { name: "3. Watchtree Nature Reserve", lat: 54.90997860874174, lng: -3.0905764048378943, description: "limited hot menu, lunch from 12pm." }, 
+    { name: "4. The Kings Head Kirby Stephen", lat: 54.46864472506169, lng: -2.4303786047828804, description: "Hot food and drink, warm space and phone charging. Open from 0830." }, 
+    { name: "5. Watchday nature reserver", lat: 54.90208328078605, lng: -3.086811718433345, description: "Hot food, drinks and warm space fromo 9am for food." }, 
+    { name: "6. The Crossings Inn", lat: 55.073850297332285, lng: -2.762715034706338, description: "from 11am."}, 
+    { name: "7. Graham Arms Inn Carlisle", lat: 55.04396214453601, lng: -2.968708689617571, description: "hot drinks and hot food, phone charging, showers (bring own towel) from 12pm to 8pm." }, 
+    { name: "8. Plough Inn Wray", lat: 54.853097790934235, lng: -2.887329331992143, description: "Hot food and drinks from 12pm." }, 
+    { name: "9. Maes Tea room and gallery", lat: 54.74860763983286, lng:  -3.161987538540455, description: "Open from 10am. Hot drinks, hot food, phone charging and showers." }, 
+    { name: "10. Hundith Hill Hotel", lat: 54.675621334599256, lng: -3.3377687907313747, description: "Hot food and drinks, warm space from 1030 - 1330" },
+    { name: "11. The Barista Wigton", lat: 54.853097790934235, lng: -3.1592409564749717, description: "Serving hot food and drink." }, 
 
-    { name: "1. The Screes Inn", lat: 54.45587337424352, lng: -3.35150170105879, description: "Free breakfasts." },
-    { name: "2. The Falcon Club Egremont", lat: 54.52139907861723, lng: -3.5357200972733116, description: "limited hot menu, lunch from 12pm." }, 
-    { name: "3. The Kings Head Kirby Stephen", lat: 54.46864472506169, lng: -2.4303786047828804, description: "Hot food and drink, warm space and phone charging. Open from 0830." }, 
-    { name: "4. Watch Tree nature reserver", lat: 54.90208328078605, lng: -3.086811718433345, description: "Hot food, drinks and warm space fromo 9am - 8:30pm" }, 
-    { name: "5. The Crossings Inn", lat: 55.073850297332285, lng: -2.762715034706338, description: "From 6pm."}, 
-    { name: "6. Maes Tea room and gallery", lat: 54.74860763983286, lng:  -3.161987538540455, description: "Open from 10am. Hot drinks, hot food, phone charging and showers." }, 
-    { name: "7. Lowther Arms Community Pub", lat: 54.83739699294534, lng: -3.4231102325885034, description: "From mid-day"  },
-    { name: "8. The Barista Wigton", lat: 54.853097790934235, lng: -3.1592409564749717, description: "Serving hot food and drink." }, 
-    { name: "9. Watermill Inn and Brewery", lat: 54.40965863751213, lng: -2.8545677450334987, description: "Serving food &  drink from 12pm - 8pm, warm spaces, showers, mobile charging." }, 
-    { name: "10. The Manor Inn and Brewing Co.", lat: 54.500161574990365, lng: -3.5904765518752515, description: "Hot food and drink from 2pm - 8pm, warm space and mobile chanring" },
-  
-    
-  //  { name: "10. Hundith Hill Hotel", lat: 54.675621334599256, lng: -3.3377687907313747, description: "Hot food and drinks, warm space from 1030 - 1330" },
-  //  { name: "8. Plough Inn Wray", lat: 54.853097790934235, lng: -2.887329331992143, description: "Hot food and drinks from 12pm." }, 
-  //  { name: "7. Graham Arms Inn Carlisle", lat: 55.04396214453601, lng: -2.968708689617571, description: "hot drinks and hot food, phone charging, showers (bring own towel) from 12pm to 8pm." }, 
-  //  { name: "1. The Commodore Inn", lat: 54.228517801041455, lng: -2.9093019885160087, description: "breakfast 9am to 10am, lunch 12pm to 2:30pm" },
-  //  { name: "12. The Old Bank Tearooms", lat: 54.80416650169004, lng: -3.3187401141001454, description: "Hot food and drinks from 1030 - 1330" },
-  //  { name: "13. Catering Van Nethertown Square", lat: 54.52139907861723, lng:  -3.5961449027139394, description: "Hot food and drinks from 12pm" },
-     // Add more locations here 
-];
-
+    { name: "12. The Old Bank Tearooms", lat: 54.80416650169004, lng: -3.3187401141001454, description: "Hot food and drinks from 1030 - 1330" },
+    { name: "13. Catering Van Nethertown Square", lat: 54.52139907861723, lng:  -3.5961449027139394, description: "Hot food and drinks from 12pm" },
+    { name: "14. Lowther Arms Community Pub", lat: 54.83739699294534, lng: -3.4231102325885034, description: "From mid-day"  },
+    { name: "15. Watermill Inn and Brewery", lat: 54.40965863751213, lng: -2.8545677450334987, description: "Serving food &  drink from 12pm - 8pm, warm spaces, showers, mobile charging." }, 
+    // Add more locations here 
+]; 
 
 let map;
 let originMarker;
 let destinationMarker;
+let locationMarkers = [];
 
 function initMap() {
     const initialLocation = { lat: 54.55110183921372, lng: -3.0835090379866132 }; 
@@ -81,7 +78,7 @@ function displayLocations() {
         const distanceInMiles = location.distance !== Infinity ? (location.distance / 1609.34).toFixed(2) : 'N/A';
         const durationInMinutes = location.duration !== Infinity ? Math.round(location.duration / 60) : 'N/A';
         const listItem = document.createElement('li');
-        listItem.innerHTML = `${location.name} - ${distanceInMiles} miles - ${durationInMinutes} mins - <i>${location.description}</i>`;
+        listItem.innerHTML = `<b>${location.name} </b> - ${distanceInMiles} miles - ${durationInMinutes} mins - <i>${location.description}</i>`;
         list.appendChild(listItem);
     });
 }
@@ -102,6 +99,11 @@ function updateMap(origin, nearestLocation) {
         url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png', // Red icon for nearest location
     };
 
+  const otherLocationsIcon = {
+    url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png', // Blue icon for other locations
+  };
+
+
     originMarker = new google.maps.Marker({
         position: origin,
         map: map,
@@ -115,6 +117,19 @@ function updateMap(origin, nearestLocation) {
         title: nearestLocation.name,
         icon: destinationIcon
     });
+
+    
+locations.forEach(location => {
+    if (location !== nearestLocation) {
+      const marker = new google.maps.Marker({
+        position: { lat: location.lat, lng: location.lng },
+        map: map,
+        title: location.name,
+        icon: otherLocationsIcon
+      });
+      locationMarkers.push(marker);
+    }
+  });
 
 
     map.setCenter(origin);
